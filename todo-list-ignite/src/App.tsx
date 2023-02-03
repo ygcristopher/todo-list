46b61fb12d8d175718034ae9696736b4417a1ee1
ygcristopher <yago1900@gmail.com>
2023-02-03 12:02:36 -0300

    finish commit

--- todo-list-ignite/src/App.tsx
+++ todo-list-ignite/src/App.tsx
@@ -1,7 +1,10 @@
 import { Header } from './components/Header/Header'
 import { Tasks } from './components/Tasks/Tasks'
 import '../src/styles/global.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+
+const LOCAL_STORAGE_KEY = "todo:savedTasks";
 
 export interface ITask {
   id: string;
@@ -10,21 +13,26 @@ export interface ITask {
 }
 
 function App() {
-  const [tasks, setTasks] = useState<ITask[]>([
-    {
-      id: "teste",
-      title: "teste",
-      isCompleted:true,
-    },
-    {
-      id: "teste 2",
-      title: "teste",
-      isCompleted:false,
-    },
-  ]);
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  function loadSavedTasks() {
+    const saved = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if(saved){
+      setTasks(JSON.parse(saved));
+    }
+  }
+
+  useEffect(() => {
+    loadSavedTasks();
+  }, [])
+
+  function setTasksAndSave(newTasks: ITask[]) {
+    setTasks(newTasks);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTasks));
+  }
 
   function addTask(taskTitle: string) {
-    setTasks([
+    setTasksAndSave([
       ...tasks,
       {
         id: crypto.randomUUID(),
@@ -36,13 +44,27 @@ function App() {
 
   function deleteTaskById(taskId: string){
     const newTasks = tasks.filter((task) => task.id != taskId);
-    setTasks(newTasks);
+    setTasksAndSave(newTasks);
+  }
+
+
+  function toggleTaskCompletedById(taskId: string) {
+    const newTasks = tasks.map(task => {
+      if(task.id == taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        }
+      }
+      return task;
+    });
+    setTasksAndSave(newTasks);
   }
 
   return (
     <>
     <Header onAddTask={addTask} />
-    <Tasks tasks={tasks} onDelete={deleteTaskById} />
+    <Tasks tasks={tasks} onDelete={deleteTaskById} onComplete={toggleTaskCompletedById}/>
     </>
   )
 }
--- todo-list-ignite/src/components/Tasks/Tasks.tsx
+++ todo-list-ignite/src/components/Tasks/Tasks.tsx
@@ -1,13 +1,16 @@
+import { TbClipboardText } from "react-icons/tb";
 import { ITask } from "../../App"
 import { Task } from '../Task/Task'
 import styles from './Tasks.module.css'
 
 interface Props {
     tasks: ITask[];
-    onDelete: (taskId, onDelete: string) => void;
+    onDelete: (taskId: string) => void;
+    onComplete: (taskId: string) => void;
+
 }
 
- export function Tasks({tasks, onDelete}: Props) {
+ export function Tasks({ tasks, onDelete, onComplete}: Props) {
     const tasksQuantity = tasks.length;
     const completedTasks = tasks.filter((task) => task.isCompleted).length;
 
@@ -28,11 +31,26 @@ interface Props {
 
             <div className={styles.list}>
                 {tasks.map((task) => (
-                    <Task key={task.id} task={task} onDelete={onDelete} />
+                <Task 
+                    key={task.id} 
+                    task={task} 
+                    onDelete={onDelete} 
+                    onComplete={onComplete} 
+                />
                 ))}
+
+
+                {tasks.length <= 0 && (
+                   <section className={styles.empty}>
+                    <TbClipboardText size={50} />
+                    <div>
+                        <p>Você ainda não tem ToDo cadastradas</p>
+                        <span>Crie tarefas e organize seus itens a fazer</span>
+                    </div>
+                   </section>
+                )}
             </div>
         </section>
     )
 }
 
-export default Tasks
